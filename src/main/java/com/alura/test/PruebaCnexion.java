fb2f8ae590a4dc850880a8e1e00c8100b937c8c8
Luismarin2510 <opraedu@gmail.com>
2023-03-26 20:36:21 -0500

    creando conexion

--- src/main/java/com/alura/test/PruebaCnexion.java
+++ src/main/java/com/alura/test/PruebaCnexion.java
@@ -1,5 +1,20 @@
 package com.alura.test;
 
-public class PruebaCnexion {
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
+public class PruebaCnexion {
+	
+	public static void main(String[] args) throws SQLException {
+		Connection con;
+		
+			con = DriverManager.getConnection(
+					"jdbc:mysql://localhost/control_de_stock?useTimeZone=true&serverTimeZone=UTC", 
+					"root", 
+					"Lm102052*");
+		
+		con.close();
+	}
+ 
 }
